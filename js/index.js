ca0d50a2c2f8552d7216d849969fb7b416e6ec96
Mukenia-Mark <Mark.mukenia@gmail.com>
2021-02-28 22:58:42 +0300

    Add what we do section

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-//  top section
+//  What we do section//
+$(document).ready(function() {
+  $(".designclickable").click(function() {
+    $("#design-showing").toggle();
+    $("#design-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".developementclickable").click(function() {
+    $("#developement-showing").toggle();
+    $("#developement-hidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".productclickable").click(function() {
+    $("#productmanagement-showing").toggle();
+    $("#productmanagement-hidden").toggle();
+  });
+});
